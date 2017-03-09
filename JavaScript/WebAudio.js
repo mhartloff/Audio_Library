5ce803afc35126c6270b1e4581fa05a43a03f86b
Matthew Hartloff <hartlomj@yahoo.com>
2017-03-08 22:33:34 -0600

    Cleanup of unused code.

--- JavaScript/WebAudio.js
+++ JavaScript/WebAudio.js
@@ -44,19 +44,14 @@ SoundSource.prototype.decode = function () {
 
         		// Change all sounds to be 2 channels with the same information in each.  This will allow us to manipulate each
 				// channel (L/R) individually.
-            //var channelLength = buffer.length;
             var origData = buffer.getChannelData(0);
-            //var origData = new Float32Array(channelLength);
-            //buffer.copyFromChannel(data, 0, 0);		// Copy data from the first channel
             var newBuffer = WebAudio.context.createBuffer(2 /* num channels */, origData.length, buffer.sampleRate);	// 2 channel buffer from mono sound
             var newData = newBuffer.getChannelData(0);
             newData.set(origData);
 				
             newData = newBuffer.getChannelData(1);
             newData.set(origData);
-            //newBuffer.copyToChannel(data, 0);
-            //newBuffer.copyToChannel(data, 1);
-
+            
             self.buffer = newBuffer;
             self.loaded = true;
         },
@@ -139,7 +134,6 @@ var WebAudio =
    context: null,
    soundSourceMap: [],        // Map of sound aliases to SoundSource objects.
 	
-	headWidth: 12 / 39.3,					// Distance between ears, in meters.
 	gainNode: null,				// Node that adjusts the universal volume
 	outputNode: null,				// Node attached to the speakers.  Attach this to the end of all node graphs of sounds.
    
@@ -165,43 +159,11 @@ var WebAudio =
 		DeviceMotion.updateCallback = function () { self.onMotionUpdate(); }
    },
 
-   getPosition: function () {
-   	return new Vector(this.position.x, this.position.y, this.position.z);
-   },
-	
-   getLeftEarInfo: function (position /* Vector */, direction /* Vector */) {
-   	var leftVec = this.getLeftVec();
-   	if (direction)
-   		direction.set(leftVec);
-   	if (position) {
-   		var pos = this.getPosition();
-   		leftVec.setLength(this.headWidth / 2.0);
-   		position.set(pos.x + leftVec.x, pos.y + leftVec.y, pos.z + leftVec.z);
-   	}
-   },
-
-   getRightEarInfo: function (position /* Vector */, direction /* Vector */) {
-   	var rightVec = this.getLeftVec();
-   	rightVec.reverse();		// now it's a right vec
-   	if (direction)
-   		direction.set(rightVec);
-   	if (position) {
-   		var pos = this.getPosition();
-   		rightVec.setLength(this.headWidth / 2.0);
-   		position.set(pos.x + rightVec.x, pos.y + rightVec.y, pos.z + rightVec.z);
-   	}
-   },
-	
    setSceneCanvas: function (canvasElement) {
    	this.sceneCanvas = new SceneCanvas(canvasElement);
    },
 
-	// Set the head width, in meters
-   setHeadWidth: function (width) {
-   	this.headWidth = width;
-   	this.updateSpatialSounds();
-   },
-
+	
 	// Load a sound file such as a .wav or .mp3.  Assign an alias for easy future reference.
    loadSoundSource: function (url, alias) {
       var newSource = new SoundSource(url, alias);
@@ -225,104 +187,7 @@ var WebAudio =
    	var source = this.get(alias);
    	if (source)
    		source.drawOnCanvas();
-   },
+   }
 
-  			
-	/* --- PRIVATE FUNCTIONS ---------------------------- */
-
-	updateSceneCanvas: function() {
-		if (sceneCanvas) {
-			sceneCanvas.drawScene();
-		}
-	},
-
-	// Add a Sound object to the list of sounds that are playing
-	addSound: function (id, sound) {
-		this.activeSounds[id] = sound;
-		//this.updateSceneCanvas();
-	},
-
-	removeSound: function (id) {
-		delete this.activeSounds[id];
-		//this.updateSceneCanvas();
-	},
 }
 
-
-/* --- SCENE CANVAS ------------------------------------------------------- */
-
-// Displays the current state of the WebAudio API as a 2D map.  The canvas is interactive with the mouse and is derived from Canvas2D.
-function SceneCanvas(canvasElement) {
-	Canvas2D.call(this, canvasElement);
-
-	this.selectedPos = null;		// The last position selected by the user, in scene space.
-	this.setProjection(0, 0, 40);
-
-	this.drawScene();
-}
-
-SceneCanvas.prototype = Object.create(Canvas2D.prototype);
-SceneCanvas.prototype.constructor = SceneCanvas;
-
-SceneCanvas.prototype.getSelectedPosition = function () {
-	return this.selectedPos;
-}
-
-SceneCanvas.prototype.drawScene = function () {
-	
-	var context = this.context;
-	this.clear('rgb(200, 200, 200)');
-
-	// Draw the axes and the tics.
-	this.drawLine(-1000, 0, 1000, 0, "rgb(50, 50, 50)");
-	this.drawLine(0, -1000, 0, 1000, "rgb(50, 50, 50)");
-
-	var tic = .25;
-	this.drawLine(-tic, 5, tic, 5, "rgb(100, 100, 100)");	// draw a tick at 5.
-	this.drawText("+5X", 4.6, -tic * 1.25);
-	this.drawLine(-tic, -5, tic, -5, "rgb(100, 100, 100)");	// draw a tick at 5.
-	
-	this.drawLine(5, -tic, 5, tic, "rgb(100, 100, 100)");	// draw a tick at 5.
-	this.drawText("+5Z", tic * 1.25, 4.9);
-	this.drawLine(-5, -tic, -5, tic, "rgb(100, 100, 100)");	// draw a tick at 5.
-		
-		
-	// Draw the listener
-	var pos = WebAudio.getPosition();
-	var dir = WebAudio.getDirection();
-	dir.setLength(1.0);
-	this.drawCircle(pos.x, pos.z, 6 /* radius */, "rgb(10, 10, 10)", "rgb(50, 50, 200)");
-	this.drawLine(pos.x, pos.z, pos.x + dir.x, pos.z + dir.z, "rgb(255, 0, 0)");
-
-	// Draw the selected position
-	if (this.selectedPos) {
-		var p = this.selectedPos;
-		this.drawLine(p.x - 0.2, p.y - 0.2, p.x + 0.2, p.y + 0.2, "rgb(200, 40, 40)");
-		this.drawLine(p.x + 0.2, p.y - 0.2, p.x - 0.2, p.y + 0.2, "rgb(200, 40, 40)");
-	}
-
-	// Draw each active sound
-	for (var soundId in WebAudio.activeSounds) {
-		if (WebAudio.activeSounds.hasOwnProperty(soundId)) {
-			var sound = WebAudio.activeSounds[soundId];
-			var pos = sound.getPosition();
-			this.drawCircle(pos.x, pos.z, 6, "rgb(10, 10, 10)", "rgb(200, 50, 50)");
-			var dir = sound.getDirection();
-			dir.mult(0.50);	// Set the length of the line
-			this.drawLine(pos.x, pos.z, pos.x + dir.x, pos.z + dir.z, "rgb(20, 20, 100)");
-		}
-	}
-},
-
-SceneCanvas.prototype.onMouseDown = function (p /* pick point */) {
-	this.selectedPos = p;
-	this.drawScene();
-}
-	
-SceneCanvas.prototype.onMouseUp = function (p) {
-	//console.log("Mouse Up: " + p.x + ", " + p.y);
-}
-
-SceneCanvas.prototype.onMouseMove = function (p) {
-	//console.log("Mouse Move: " + p.x + ", " + p.y);
-}
